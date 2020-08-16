a77a79cde7260b801f95a9ad72c9737dd0548db5
Jual Ahmed <md.jual.ah@gmail.com>
2020-08-16 19:45:03 +0600

    sdf

---
+++ app/Package.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Package extends Model
+{
+    protected $fillable = [
+        'name',
+        'buy_price',
+        'sale_price',
+        'product_id'
+    ];
+}
