1ab304b75de4f971a5af4cde5807d17cd60d550e
Jual Ahmed <md.jual.ah@gmail.com>
2020-09-01 23:34:15 +0600

    sf

--- resources/js/pages/auth/password/email.vue
+++ resources/js/pages/auth/password/email.vue
@@ -1,31 +1,31 @@
 <template>
 <div class="row" style="min-height: 460px">
-    <div class="col-4 m-auto  p-2 md:p-4">
-        <card class="justify-center shadow-lg hover:shadow-xl  m-auto border-2 w-full md:w-4/12 xl:w-3/12 p-2 md:p-4 m-2 md:m-0" style="margin:auto;">
-            <h4 class="text-xl font-bold text-center">Reset Password</h4>
-            <form @submit.prevent="send" @keydown="form.onKeydown($event)">
-              <alert-success :form="form" :message="status" style="color: red;"/>
+		<div class="col-4 m-auto  p-2 md:p-4">
+				<card class="justify-center shadow-lg hover:shadow-xl  m-auto border-2 w-full md:w-4/12 xl:w-3/12 p-2 md:p-4 m-2 md:m-0" style="margin:auto;">
+						<h4 class="text-xl font-bold text-center">Reset Password</h4>
+						<form @submit.prevent="send" @keydown="form.onKeydown($event)">
+							<alert-success :form="form" :message="status" style="color: red;"/>
 
-              <!-- Email -->
-              <div class="form-group row">
-                <label class="col-md-3 col-form-label text-md-right font-bold">{{ $t('email') }}</label>
-                <div class="col-md-7">
-                  <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="shadow rounded w-full py-2 px-3 text-grey-darker mb-3"  type="email" name="email">
-                  <has-error :form="form" field="email" style="color: red;"/>
-                </div>
-              </div>
+							<!-- Email -->
+							<div class="form-group row">
+								<label class="col-md-3 col-form-label text-md-right font-bold">{{ $t('email') }}</label>
+								<div class="col-md-7">
+									<input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="shadow rounded w-full py-2 px-3 text-grey-darker mb-3"  type="email" name="email">
+									<has-error :form="form" field="email" style="color: red;"/>
+								</div>
+							</div>
 
-              <!-- Submit Button -->
-              <div class="form-group row">
-                <div class="col-md-9 ml-md-auto">
-                  <button :loading="form.busy"  class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-3">
-                    {{ $t('send_password_reset_link') }}
-                  </button>
-                </div>
-              </div>
-            </form>
-        </card>
-    </div>
+							<!-- Submit Button -->
+							<div class="form-group row">
+								<div class="col-md-9 ml-md-auto">
+									<button :loading="form.busy"  class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-3">
+										{{ $t('send_password_reset_link') }}
+									</button>
+								</div>
+							</div>
+						</form>
+				</card>
+		</div>
 </div>
 </template>
 
@@ -33,27 +33,27 @@
 import Form from 'vform'
 
 export default {
-  middleware: 'guest',
+	middleware: 'guest',
 
-  metaInfo () {
-    return { title: this.$t('reset_password') }
-  },
+	metaInfo () {
+		return { title: this.$t('reset_password') }
+	},
 
-  data: () => ({
-    status: '',
-    form: new Form({
-      email: ''
-    })
-  }),
+	data: () => ({
+		status: '',
+		form: new Form({
+			email: ''
+		})
+	}),
 
-  methods: {
-    async send () {
-      const { data } = await this.form.post('/api/password/email')
+	methods: {
+		async send () {
+			const { data } = await this.form.post('/api/password/email')
 
-      this.status = data.status
+			this.status = data.status
 
-      this.form.reset()
-    }
-  }
+			this.form.reset()
+		}
+	}
 }
 </script>
--- resources/js/pages/auth/password/reset.vue
+++ resources/js/pages/auth/password/reset.vue
@@ -1,49 +1,49 @@
 <template>
-	<div class="row">
-		<div class="col-lg-8 m-auto">
-			<card :title="$t('reset_password')" class="justify-center shadow-lg hover:shadow-xl  mx-auto border-2 w-full md:w-6/12 xl:w-6/12 p-2 md:p-4 m-2 md:m-0">
-				<form @submit.prevent="reset" @keydown="form.onKeydown($event)">
-					<alert-success :form="form" :message="status" />
+<div class="row" style="min-height: 460px">
+	<div class="col-4 m-auto  p-2 md:p-4">
+		<card class="justify-center shadow-lg hover:shadow-xl  m-auto border-2 w-full md:w-4/12 xl:w-3/12 p-2 md:p-4 m-2 md:m-0" style="margin:auto;">
+			<form @submit.prevent="reset" @keydown="form.onKeydown($event)">
+				<alert-success class="text-red-600" :form="form" :message="status" />
 
-					<!-- Email -->
-					<div class="form-group row">
-						<label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
-						<div class="col-md-7">
-							<input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email" readonly>
-							<has-error :form="form" field="email" />
-						</div>
+				<!-- Email -->
+				<div class="form-group row">
+					<label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
+					<div class="col-md-7">
+						<input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }"  class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" type="email" name="email" readonly>
+						<has-error class="text-red-600" :form="form" field="email" />
 					</div>
+				</div>
 
-					<!-- Password -->
-					<div class="form-group row">
-						<label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
-						<div class="col-md-7">
-							<input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" type="password" name="password">
-							<has-error :form="form" field="password" />
-						</div>
+				<!-- Password -->
+				<div class="form-group row">
+					<label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
+					<div class="col-md-7">
+						<input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }"  class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" type="password" name="password">
+						<has-error class="text-red-600" :form="form" field="password" />
 					</div>
+				</div>
 
-					<!-- Password Confirmation -->
-					<div class="form-group row">
-						<label class="col-md-3 col-form-label text-md-right">{{ $t('confirm_password') }}</label>
-						<div class="col-md-7">
-							<input v-model="form.password_confirmation" :class="{ 'is-invalid': form.errors.has('password_confirmation') }" class="form-control" type="password" name="password_confirmation">
-							<has-error :form="form" field="password_confirmation" />
-						</div>
+				<!-- Password Confirmation -->
+				<div class="form-group row">
+					<label class="col-md-3 col-form-label text-md-right">{{ $t('confirm_password') }}</label>
+					<div class="col-md-7">
+						<input v-model="form.password_confirmation" :class="{ 'is-invalid': form.errors.has('password_confirmation') }"  class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" type="password" name="password_confirmation">
+						<has-error class="text-red-600" :form="form" field="password_confirmation" />
 					</div>
+				</div>
 
-					<!-- Submit Button -->
-					<div class="form-group row">
-						<div class="col-md-9 ml-md-auto">
-							<v-button :loading="form.busy">
-								{{ $t('reset_password') }}
-							</v-button>
-						</div>
+				<!-- Submit Button -->
+				<div class="form-group row">
+					<div class="col-md-9 ml-md-auto">
+						<button :loading="form.busy" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-3">
+							{{ $t('reset_password') }}
+						</button>
 					</div>
-				</form>
-			</card>
-		</div>
+				</div>
+			</form>
+		</card>
 	</div>
+</div>
 </template>
 
 <script>
