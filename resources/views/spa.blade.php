1ffe215a3165766b3c67d16a64031e7b24f6b662
Jual Ahmed <md.jual.ah@gmail.com>
2020-09-14 02:12:43 +0600

    df

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -18,6 +18,19 @@ $config = [
   <title>{{ config('app.name') }}</title>
 
   <link rel="stylesheet" href="{{ mix('dist/css/main.css') }}">
+    <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5f5e1eeef0e7167d000fe592/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
 </head>
 <body>
   <div id="app"></div>
