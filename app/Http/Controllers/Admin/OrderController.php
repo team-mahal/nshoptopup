b5b6bf36e3279d37f2acbd4f3897edf9a8cd4a7b
Jual Ahmed <md.jual.ah@gmail.com>
2020-09-19 02:13:05 +0600

    df

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -112,7 +112,7 @@ class OrderController extends Controller
 
     public function gamename(Request $request,$id)
     {
-        $data = Order::orderBy('id', 'DESC')->where('email', $id)->where('gamename','!=','ID Code')->where('type','ID Code')->first();
+        $data = Order::orderBy('id', 'DESC')->where('email', $id)->where('password','!=','ID Code')->where('type','ID Code')->first();
         return response()->json($data, 200);
     }
 
