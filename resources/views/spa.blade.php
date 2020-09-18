0803871bf2c21aaaef6f6d232b15c0ddfbbaac4d
Jual Ahmed <md.jual.ah@gmail.com>
2020-09-19 01:57:37 +0600

    sdf

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -20,7 +20,7 @@ $config = [
   <link rel="stylesheet" href="{{ mix('dist/css/main.css') }}">
     <!--Start of Tawk.to Script-->
   <script type="text/javascript">
-  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); 
   (function(){
   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
   s1.async=true;
