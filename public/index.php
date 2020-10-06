36e39f55aa0a2dd9943852d21546066778422d1a
Jual Ahmed <md.jual.ah@gmail.com>
2020-10-06 18:58:47 +0600

    df

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  *
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-define('LARAVEL_START', microtime(true));
+// define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +19,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+// require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// $app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -47,12 +47,14 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+// $response = $kernel->handle(
+//     $request = Illuminate\Http\Request::capture()
+// );
 
-$response->send();
+// $response->send();
 
-$kernel->terminate($request, $response);
+// $kernel->terminate($request, $response);
+
+echo "Coming Soon";
